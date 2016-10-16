a7c2ddad4e47167927dd4746bf783b0d48aae5f3
Olgert Denas <gertidenas@gmail.com>
2016-10-16 01:14:15 -0700

    v1 . no suffix trees topology

--- fast_ms/fd_ms/fd_ms.cpp
+++ fast_ms/fd_ms/fd_ms.cpp
@@ -26,31 +26,48 @@ struct interval bstep(csa_wt<> index, uint8_t lb, uint8_t ub, uint8_t c){
 
 
 int main(int argc, char **argv){
-    //char t[] = {'a', 'b', 'b', 'a', 'b', 'a'};
     int_vector<8> t = {'a', 'b', 'b', 'a', 'b', 'a'};
-    
-    
-    //uint8_t w_idx, w_len;
-    //uint8_t k = t.size() - 1;
-    //uint8_t c;
+    int_vector<8> runs = {0, 0, 0, 0, 0, 0}; // the runs vector
+
+    uint8_t w_idx, w_len;
+    uint8_t k, c;
+    struct interval I;
 
 	csa_wt<> sa_of_s;
-	construct_im(sa_of_s, "abbaba", 1);
+	construct_im(sa_of_s, "ab", 1);
 
-    
-    int_vector<8> runs; // the runs vector
+    k = t.size();
+    w_idx = k - 1;
+    w_len = 1;
+    I.lb = I.ub = 1;
+    while(--k > 0){
+        c = t[k-1];
+        cout << "k = " << (int) k << endl;
+        cout << "w = (" << (int) w_idx << ", " << (int) w_len << ")" << endl;
+        cout << "c = " << c << endl;
+        
+        I = bstep(sa_of_s, I.lb, I.ub, t[k - 1]);
+        cout << "{" << (int) I.lb << ", " << (int) I.ub << "}" << endl;
 
-    for(int k = 0; k < sa_of_s.sigma; k++){
-        cout << k << sa_of_s.comp2char[k] << endl;
-        cout << "C[" << k << "] = " << sa_of_s.C[k] << endl;
+        if(I.lb > I.ub){
+            runs[k] = 0;
+            I = bstep(sa_of_s, 0, sa_of_s.size() - 1, c);
+            w_len = 1;
+            cout << "-" << endl;
+        } else {
+            w_len += I.ub - I.lb + 1;
+            runs[k] = 1;
+            cout << "+" << endl;
+        }
+        w_idx--;
         cout << endl;
     }
     
-    struct interval I = bstep(sa_of_s, 0, 5, 'b');
-    cout << "{" << I.lb << ", " << I.ub << "}" << endl;
-    I = bstep(sa_of_s, I.lb, I.ub, 'b');
-    cout << "{" << I.lb << ", " << I.ub << "}" << endl;
-    
+    //struct interval I = bstep(sa_of_s, 0, 5, 'b');
+    //cout << "{" << (int) I.lb << ", " << (int) I.ub << "}" << endl;
+    //I = bstep(sa_of_s, I.lb, I.ub, 'b');
+    //cout << "{" << (int) I.lb << ", " << (int) I.ub << "}" << endl;
+
 
 //	cout << " i SA ISA PSI LF BWT   T[SA[i]..SA[i]-1]" << endl;
 //	csXprintf(cout, "%2I %2S %3s %3P %2p %3B   %:3T", sa_of_s);
