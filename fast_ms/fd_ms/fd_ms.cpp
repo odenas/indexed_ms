e936c388819531cf4a74ecca8ec68f1dacaf25fa
Olgert Denas <gertidenas@gmail.com>
2016-10-24 23:31:20 -0700

    implemented phase 2 (ugly code)

--- fast_ms/fd_ms/fd_ms.cpp
+++ fast_ms/fd_ms/fd_ms.cpp
@@ -10,23 +10,31 @@
 #include <sdsl/wavelet_trees.hpp>
 #include <sdsl/bp_support.hpp>
 #include <sdsl/csa_wt.hpp>
+#include <sdsl/bit_vectors.hpp>
+
 
 using namespace std;
 using namespace sdsl;
 
+
+struct range{
+    uint8_t w_start, w_len; // word coords
+    uint8_t iw, jw;         // interval coords
+};
+
 struct interval{
-    uint8_t lb = 0, ub = 0;
+    int lb = 0, ub = 0;
 };
 
 struct omega {
-    uint8_t idx = 0, len = 0;
+    int idx = 0, len = 0;
 };
 
 struct interval bstep(csa_wt<> sa, uint8_t lb, uint8_t ub, uint8_t c){
     struct interval i;
     
-    i.lb = sa.C[sa.char2comp[c]] + sa.wavelet_tree.rank(lb, c);
-    i.ub = sa.C[sa.char2comp[c]] + sa.wavelet_tree.rank(ub + 1, c) - 1;
+    i.lb = (int)(sa.C[sa.char2comp[c]] + sa.wavelet_tree.rank(lb, c));
+    i.ub = (int)(sa.C[sa.char2comp[c]] + sa.wavelet_tree.rank(ub + 1, c) - 1);
     return i;
 }
 
@@ -50,7 +58,7 @@ void output_interval(const struct interval I){
 
 struct interval bstep_along(csa_wt<> sa, int_vector<8> t, struct omega w){
     struct interval i;
-    i.ub = sa.size() - 1;
+    i.ub = (int) sa.size() - 1;
     uint8_t k = w.len;
     
     while(k-- > 0)
@@ -58,25 +66,37 @@ struct interval bstep_along(csa_wt<> sa, int_vector<8> t, struct omega w){
     return i;
 }
 
+struct interval bstep_along_rev(csa_wt<> sa, int_vector<8> t, struct omega w){
+    struct interval i;
+    i.ub = (int) (sa.size() - 1);
+    uint8_t k = 0;
+    
+    while(k++ < w.len)
+        i = bstep(sa, i.lb, i.ub, t[k]);
+    return i;
+}
 
-int main(int argc, char **argv){
-    int_vector<8> t = {'a', 'b', 'b', 'a', 'b', 'a'};
-    int_vector<8> runs = {0, 0, 0, 0, 0, 0}; // the runs vector
 
+void init_interval(struct interval *I, cst_sct3<> *st, const uint8_t c){
+    uint8_t cc = st->csa.char2comp[c];
+    
+    I->lb = (int) st->csa.C[cc];
+    I->ub = (int) (st->csa.C[cc + 1] - 1);
+}
+
+void phase1(uint *runs, cst_sct3<> *st_of_s_ptr, int_vector<8> *t_ptr){
     uint8_t k, c;
     struct interval I;
-    struct omega w, a;
+    struct omega w;
     cst_sct3<>::node_type v;
-
-    cst_sct3<> st_of_s;
-    construct_im(st_of_s, "ab", 1);
-
+    cst_sct3<> st_of_s = (*st_of_s_ptr);
+    int_vector<8> t = (*t_ptr);
+    
     k = t.size();
     // start with the single char interval
     c = t[k - 1]; w.idx = k - 1; w.len = 1;
     // initialize to the interval of t[k - 1]
-    I.lb = st_of_s.csa.C[st_of_s.csa.char2comp[c]];
-    I.ub = st_of_s.csa.C[st_of_s.csa.char2comp[c] + 1] - 1;
+    init_interval(&I, &st_of_s, c);
     v = st_of_s.child(st_of_s.root(), c);
 
     while(--k > 0){
@@ -84,15 +104,15 @@ int main(int argc, char **argv){
         cout << "[" << (int) k << "]" << endl;
         output_omega(w, t);
         cout << "c = " << c << endl;
-
+        
         I = bstep(st_of_s.csa, I.lb, I.ub, t[k - 1]);
         output_interval(I);
-
+        
         if(I.lb > I.ub){
             runs[k] = 0;
             // update I to the parent of the proper locus of w
             cout << "|parent(v)| = " << (int) st_of_s.depth(st_of_s.parent(v)) << endl;
-            w.len = st_of_s.depth(st_of_s.parent(v));
+            w.len = (int) st_of_s.depth(st_of_s.parent(v));
             I = bstep_along(st_of_s.csa, t, w);
             I = bstep(st_of_s.csa, I.lb, I.ub, t[k - 1]);
             cout << "-" << endl;
@@ -104,10 +124,105 @@ int main(int argc, char **argv){
         w.idx--; w.len++;
         cout << endl;
     }
+}
+
+uint matching_stat(bit_vector ms, uint k){
+    if(k == -1)
+        return (uint) 1;
+    return ((uint) bit_vector::select_1_type (&ms)(k + 1)) - (2 * k);
+}
+
+void output_ms(bit_vector ms, int ms_idx){
+    cout << "ms: ";
+    for(int i=0; i<ms.size(); i++)
+        cout << ms[i];
+    cout << endl << "    ";
+    for(int i=0; i<ms.size(); i++)
+        cout << (i == ms_idx ? "*" : " ");
+    cout << endl;
+}
+
+void phase2(uint *runs, bit_vector *ms, cst_sct3<> *st_of_s_ptr, int_vector<8> *t_ptr){
+    int k, c, h_star = 0, k_prim, ms_idx = 0;
+    struct interval I;
+    struct omega w;
+    cst_sct3<>::node_type v;
+    cst_sct3<> st_of_s = (*st_of_s_ptr);
+    int_vector<8> t = (*t_ptr);
     
-    for(int i = 1; i < runs.size(); i++){
-        cout << "runs[" << i << "] = " << (int) runs[i] << endl;
+
+
+    k = 0, c = t[0];
+    // initialize to the interval of t[k - 1]
+    init_interval(&I, &st_of_s, c);
+    w.idx = 0, w.len = 1;
+    v = st_of_s.child(st_of_s.root(), c);
+
+    while(h_star < t.size() - 1){
+        output_ms(*ms, ms_idx);
+        for(h_star = k + 1; I.lb <= I.ub; ){
+            I = bstep(st_of_s.csa, I.lb, I.ub, t[h_star]);
+            if(I.lb <= I.ub){
+                w.len++;
+                v = st_of_s.wl(v, c);
+                h_star ++;
+            }
+        }
+        // I.lb > I.ub. MS[k] = h_star - k
+        for(int i = 0; i < h_star - k - matching_stat(*ms, k - 1) + 1; i++)
+            (*ms)[ms_idx++] = 0;
+        (*ms)[ms_idx++] = 1;
+
+        k_prim = w.idx + w.len;
+        do {
+            v = st_of_s.parent(v);
+            w.len = (int)st_of_s.depth(v);
+            I = bstep_along_rev(st_of_s.csa, t, w);
+            I = bstep(st_of_s.csa, I.lb, I.ub, t[h_star]);
+            k_prim -= w.len;
+        } while(I.lb > I.ub);
+        
+
+        for(int i=k+1; i<=k_prim - 1; i++)
+            (*ms)[ms_idx++] = 1;
+        
+        //update omega
+        w.idx = k_prim; w.len = h_star - k_prim + 1;
+        k = k_prim;
     }
+    output_ms(*ms, ms_idx);
+    h_star = k + 1;
+    for(int i = 0; i < h_star - k - matching_stat(*ms, k - 1) + 1; i++)
+        (*ms)[ms_idx++] = 0;
+    (*ms)[ms_idx++] = 1;
+    output_ms(*ms, ms_idx);
+}
+
+
+int main(int argc, char **argv){
+    int_vector<8> t = {'a', 'b', 'b', 'a', 'b', 'a'};
+    bit_vector ms(2 * t.size());
+    /*
+    ms[2] = 1; ms[3] = 1; ms[5] = 1; ms[8] = 1; ms[9] = 1; ms[11] = 1;
+    for(int i=0; i<ms.size(); i++)
+        cout << "ms["<< i << "] = " << ms[i] << endl;
+    for(int i=-1; i<6; i++)
+        cout << "MS["<< i << "] = " << (int) matching_stat(ms, i) << endl;
+    */
+    uint runs[t.size()];
+    cst_sct3<> st_of_s, st_of_s_rev;
+    construct_im(st_of_s, "ab", 1);
+    construct_im(st_of_s_rev, "ba", 1);
+
+    phase1(runs, &st_of_s, &t);
+    phase2(runs, &ms, &st_of_s_rev, &t);
+    for(int i=0; i<t.size(); i++){
+        cout << "MS[" << i << "] = " << (int) matching_stat(ms, i) << endl;
+    }
+
+    //for(int i = 1; i < t.size(); i++){
+    //    cout << "runs[" << i << "] = " << (int) runs[i] << endl;
+    //}
 
 //	cout << " i SA ISA PSI LF BWT   T[SA[i]..SA[i]-1]" << endl;
 //	csXprintf(cout, "%2I %2S %3s %3P %2p %3B   %:3T", sa_of_s);
