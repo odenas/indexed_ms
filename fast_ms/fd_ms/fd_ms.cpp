b25045c3f61abc991fb35d4b1a95e337bff725ba
Olgert Denas <gertidenas@gmail.com>
2016-10-30 15:52:38 -0700

    remove dependence from node depth

--- fast_ms/fd_ms/fd_ms.cpp
+++ fast_ms/fd_ms/fd_ms.cpp
@@ -116,21 +116,57 @@ Wstate init_state(int idx, cst_sct3<> *stree, char c){
     return state;
 }
 
-void phase1(bit_vector runs, cst_sct3<> *st_of_s_ptr, string t, bool verbose){
-    cst_sct3<> st_of_s = (*st_of_s_ptr);
+bit_vector phase1(cst_sct3<> *st_of_s_ptr, string t, bool verbose){
+    cst_sct3<> st_of_s = *st_of_s_ptr;
+    bit_vector runs(t.size());
     uint8_t k = t.size(), c = t[k - 1];
-    Wstate state = init_state(k - 1, st_of_s_ptr, c);
-    
+
+    Interval I;
+    I.lb = (int) st_of_s.csa.C[st_of_s.csa.char2comp[c]];
+    I.ub = (int) st_of_s.csa.C[st_of_s.csa.char2comp[c] + 1] - 1; // interval
+
+    cst_sct3<>::node_type v = st_of_s.child(st_of_s.root(), c); // stree node
+
     while(--k > 0){
         c = t[k-1];
 
-        state.I = bstep(st_of_s.csa, state.I.lb, state.I.ub, t[k - 1]);
+        I = bstep(st_of_s.csa, I.lb, I.ub, c);
+        if(I.lb > I.ub){
+            runs[k] = 0;
+            // update I to the parent of the proper locus of w until we can extend by 'c'
+            do{
+                v = st_of_s.parent(v);
+                I.lb = (int) v.i; I.ub = (int) v.j;
+                I = bstep(st_of_s.csa, I.lb, I.ub, c);
+            } while(I.lb > I.ub);
+            v = st_of_s.wl(v, c); // update v
+        } else {
+            v = st_of_s.wl(v, c); // update v
+            runs[k] = 1;
+        }
+        output_partial_vec(runs, k, "runs", verbose);
+    }
+    return runs;
+}
+
+void _phase1(bit_vector runs, cst_sct3<> *st_of_s_ptr, string t, bool verbose){
+    cst_sct3<> st_of_s = (*st_of_s_ptr);
+    uint8_t k = t.size(), c = t[k - 1];
+    Wstate state = init_state(k - 1, st_of_s_ptr, c);
+
+    while(--k > 0){
+        c = t[k-1];
+
+        state.I = bstep(st_of_s.csa, state.I.lb, state.I.ub, c);
         if(state.I.lb > state.I.ub){
             runs[k] = 0;
             // update I to the parent of the proper locus of w until we can extend by 'c'
             do{
                 state.v = st_of_s.parent(state.v);
-                state.w.len = (int)st_of_s.depth(state.v);
+                //st_of_s.rightmost_leaf(state.v);
+                //st_of_s.leftmost_leaf(state.v);
+
+                state.w.len = (int)st_of_s.depth(state.v); // how to update this?
                 state.I = bstep_along(st_of_s.csa, t, state.w);
                 state.I = bstep(st_of_s.csa, state.I.lb, state.I.ub, c);
             } while(state.I.lb > state.I.ub);
@@ -143,8 +179,64 @@ void phase1(bit_vector runs, cst_sct3<> *st_of_s_ptr, string t, bool verbose){
     }
 }
 
+int find_k_prim(bit_vector runs, int k){
+    size_t total_zeros = rank_support_v<0>(&runs)(runs.size());
+    size_t zeros = rank_support_v<0>(&runs)(k+1);
+    if(total_zeros == zeros)
+        return (int) (runs.size());
+    bit_vector::select_0_type b_sel(&runs);
+    return (int) b_sel(zeros + 1);
+}
 
 void phase2(bit_vector runs, bit_vector *ms, cst_sct3<> *st_of_s_ptr, string t, bool verbose){
+    cst_sct3<> st_of_s = (*st_of_s_ptr);
+    uint8_t k = 0, c = t[k], h_star = k + 1, k_prim = k, ms_idx = 0;
+
+    Interval I;
+    I.lb = (int) st_of_s.csa.C[st_of_s.csa.char2comp[c]];
+    I.ub = (int) st_of_s.csa.C[st_of_s.csa.char2comp[c] + 1] - 1; // interval
+
+    cst_sct3<>::node_type v = st_of_s.child(st_of_s.root(), c); // stree node
+
+
+    while(k < t.size()){
+        output_partial_vec(*ms, ms_idx, "ms", verbose);
+        for(; I.lb <= I.ub && h_star < t.size(); ){
+            c = t[h_star];
+            I = bstep(st_of_s.csa, I.lb, I.ub, c);
+            if(I.lb <= I.ub){
+                v = st_of_s.wl(v, c);
+                h_star ++;
+            }
+        }
+        for(int i = 0; i < h_star - k - MS(*ms, k - 1) + 1; i++)
+            (*ms)[ms_idx++] = 0;
+        if(h_star - k - MS(*ms, k - 1) + 1 > 0)
+            (*ms)[ms_idx++] = 1;
+
+        if(h_star < t.size()){
+            do {
+                v = st_of_s.parent(v);
+                I.lb = (int) v.i; I.ub = (int) v.j;
+                I = bstep(st_of_s.csa, I.lb, I.ub, t[h_star]);
+            } while(I.lb > I.ub);
+            h_star = h_star + 1;
+        }
+        // k_prim: index of the first zero to the right of k in runs
+        k_prim = find_k_prim(runs, k);
+
+        for(int i=k+1; i<=k_prim - 1; i++)
+            (*ms)[ms_idx++] = 1;
+
+        // update v
+        v = st_of_s.wl(v, c);
+        k = k_prim;
+    }
+    output_partial_vec(*ms, ms_idx, "ms", verbose);
+}
+
+
+void _phase2(bit_vector runs, bit_vector *ms, cst_sct3<> *st_of_s_ptr, string t, bool verbose){
     cst_sct3<> st_of_s = (*st_of_s_ptr);
     int k = 0, c = t[k], h_star = 0, k_prim = 0, ms_idx = 0;
     Wstate state = init_state(k, st_of_s_ptr, c);
@@ -197,10 +289,12 @@ bit_vector compute_ms(string T, string S, bool verbose){
         Srev[S.length() - i - 1] = S[i];
 
     bit_vector ms(2 * T.length());
-    bit_vector runs(T.length());
+    //bit_vector runs(T.length());
     cst_sct3<> st_of_s, st_of_s_rev;
     construct_im(st_of_s, S, 1);
     construct_im(st_of_s_rev, Srev, 1);
+
+    st_of_s.bp_support.enclose(0);
 
     if(verbose){
         cout << " i SA ISA PSI LF BWT   T[SA[i]..SA[i]-1]" << endl;
@@ -209,9 +303,22 @@ bit_vector compute_ms(string T, string S, bool verbose){
         csXprintf(cout, "%2I %2S %3s %3P %2p %3B   %:3T", st_of_s_rev.csa);
     }
 
-    phase1(runs, &st_of_s, T, verbose);
+    bit_vector runs = phase1(&st_of_s, T, verbose);
+    output_partial_vec(runs, 0, "runs", verbose);
     if(verbose)
         cout << endl;
+
+/*
+    for (int a = 1; a <= T.size(); a++){
+        size_t zeros = rank_support_v<0>(&runs)(a);
+        cout << zeros << " zeros in runs[0.."<< a << "]" << endl;
+        for(int i=0; i<a; i++)
+            cout << "runs[" << i  << "] = " << runs[i] << endl;
+        for(int i=1; i<=zeros; i++)
+            cout << i << " " << ((uint) bit_vector::select_0_type (&runs)(i + 1)) << " ";
+        cout << endl;
+    }
+*/
     phase2(runs, &ms, &st_of_s_rev, T, verbose);
     if(verbose){
         for(int i=0; i<T.length(); i++)
@@ -251,8 +358,8 @@ int main(int argc, char **argv){
             cout << MS(ms, i);
         cout << endl;
     } else {
-        string T {"bbab"};
-        string S {"acabb"};
+        string T {"babcbacba"};
+        string S {"bcbabaacacb"};
         ms = compute_ms(T, S, 1);
         cout << T + " " + S + " ";
         for (int i = 0; i < T.length(); i++)
