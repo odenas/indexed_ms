a1b88a8bf81b36919b0fc15e7ac66423406c4255
Olgert Denas <gertidenas@gmail.com>
2017-01-28 19:54:07 -0800

    interim re-analysis of lazy vs. non-lazy

---
+++ fast_ms/lazy_wl_performance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lazy_wl_performance
+//
+//  Created by denas on 1/28/17.
+//  Copyright © 2017 denas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
