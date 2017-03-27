0709de2f2d895cf3aba5077fef52127f44e8bf47
Olgert Denas <gertidenas@gmail.com>
2017-03-26 18:31:18 -0700

    multithreaded lazy-vs-nonlazy run

--- tests/utils.R
+++ tests/utils.R
@@ -12,15 +12,22 @@ read_ds <- function(fname, label=NA){
     read.csv(fname, header = TRUE, stringsAsFactors = FALSE)
 }
 
+sorted_bpaths <- function(b_path){
+  b_path <- as.character(levels(factor(b_path)))
+  data.frame(b_path = b_path,
+             mu_nr = as.numeric(simplify2array(lapply(strsplit(b_path, "_", fixed=TRUE), function(l) ifelse(length(l) == 4, l[[4]], NA)))),
+             inp_type = sapply(b_path, function(s) ifelse(substr(s, 5, 6) == "1G", "G", "M")))
+}
+
 read_time_ds <- function(path = 'lazy_vs_nonlazy_data/lazy_vs_nonlazy.csv',
-                         items=c("ms_alg", "runs_alg")){
+                         items=c("ms_bvector", "runs_bvector")){
   (read_ds(path) %>%
      filter(measuring == "time", item %in% items) %>%
      select(len_s, len_t, value, item, label, b_path))
 }
 
 read_lazy_call_cnt <- function(path = 'lazy_vs_nonlazy_data/lazy.csv'){
-  lazy_calls <- sapply(1:3000, function(i) sprintf("ms_consecutive_lazy_wl_calls%d", i))
+  lazy_calls <- sapply(1:3000, function(i) sprintf("consecutive_lazy_wl_calls%d", i))
   ds <- (read_ds(path) %>%
            filter(item %in% lazy_calls, label=="lazy") %>%
            mutate(count = value) %>%
@@ -29,7 +36,7 @@ read_lazy_call_cnt <- function(path = 'lazy_vs_nonlazy_data/lazy.csv'){
                      len_t = mean(len_t),
                      value = mean(value),
                      count = mean(count)))
-  ds$nwlcalls = as.numeric(sapply(ds$item, function(s) substr(s, 29, 300)))
+  ds$nwlcalls = as.numeric(sapply(ds$item, function(s) substr(s, 26, 300)))
   ds
 }
 
