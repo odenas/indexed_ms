9735dae8ee4ee0061310be503a5e1b8a1764bb4f
Olgert Denas <gertidenas@gmail.com>
2017-05-09 22:00:43 -0700

    double_rank_fail_mrep uses the maxrep vector

---
+++ fast_ms/compute_maxrep/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  compute_maxrep
+//
+//  Created by denas on 5/9/17.
+//  Copyright © 2017 denas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
