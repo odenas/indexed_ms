9f081cdd01b55e4f5bfb4e430daa2d7c023bab5d
Olgert Denas <gertidenas@gmail.com>
2017-05-15 19:56:18 -0700

    fixed issue 20

--- fast_ms/fd_ms/runs_and_ms_algorithms.hpp
+++ fast_ms/fd_ms/runs_and_ms_algorithms.hpp
@@ -39,6 +39,24 @@ namespace fdms {
         return __k;
     }
 
+    Interval _bstep_interval(StreeOhleb<>& st_, Interval& cur_i, const char_type c){
+        int cc = st_.csa.char2comp[c];
+        return std::make_pair(st_.csa.C[cc] + st_.csa.bwt.rank(cur_i.first, c),
+                              st_.csa.C[cc] + st_.csa.bwt.rank(cur_i.second + 1, c) - 1);
+    }
+
+    Interval bstep_on_interval(StreeOhleb<>& st_, Interval I, const int cc){
+        I.first += st_.csa.C[cc];
+        I.second += (st_.csa.C[cc] - 1);
+        return I;
+    }
+
+    Interval init_interval(StreeOhleb<>& st_, const char c){
+        int cc = st_.csa.char2comp[c];
+        return std::make_pair(st_.csa.C[cc], st_.csa.C[cc + 1] - 1);
+    }
+
+
     void resize_ms__(sdsl::bit_vector &ms_, float factor, size_type max_size){
         assert(factor > 1);
         size_type new_size = ms_.size() * factor;
@@ -64,12 +82,16 @@ namespace fdms {
         size_type first_fail = 0, last_fail = 0;
         node_type last_fail_node = v;
 
-        size_type k = to, c = t[k - 1];
+        size_type k = to;
+        char_type c = t[k - 1];
         bool idx_set = false;
+        Interval I = init_interval(st, c);
 
         while(--k > from){
             c = t[k-1];
-            if(st.is_root(st.double_rank_fail_wl(v, c))){ // empty
+            I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+
+            if(I.first > I.second){ // empty
                 if(!idx_set){ // first failing wl()
                     first_fail = k;
                     idx_set = true;
@@ -100,10 +122,12 @@ namespace fdms {
 
         size_type k = to, c = t[k - 1];
         bool idx_set = false;
+        Interval I = init_interval(st, c);
 
         while(--k > from){
             c = t[k-1];
-            if(st.is_root(st.double_rank_nofail_wl(v, c))){ // empty
+            I = bstep_on_interval(st, st.csa.bwt.double_rank(v.i, v.j + 1, c), st.csa.char2comp[c]);
+            if(I.first > I.second){ // empty
                 if(!idx_set){ // first failing wl()
                     first_fail = k;
                     idx_set = true;
@@ -133,18 +157,18 @@ namespace fdms {
                                         const size_type from, const size_type to){
         size_type k = from, h_star = k + 1, h = h_star, h_star_prev = h_star, k_prim, ms_idx = 0, ms_size = t.size();
         uint8_t c = t[k];
-        node_type v = st.double_rank_fail_wl(st.root(), c), u = v;
+        node_type v = st.double_rank_fail_wl(st.root(), c);
+        Interval I = init_interval(st, c);
 
         while(k < to){
             h = h_star;
             h_star_prev = h_star;
-            u = v;
 
-            while((!st.is_root(u)) && h_star < ms_size){
+            while(I.first <= I.second && h_star < ms_size){
                 c = t[h_star];
-                u = st.double_rank_fail_wl(v, c);
-                if(!st.is_root(u)){
-                    v = u;
+                I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                if(I.first <= I.second){
+                    v = st.double_rank_fail_wl_mrep(v, c, (maxrep[v.i] == maxrep[v.j] == 1));
                     h_star += 1;
                 }
             }
@@ -156,12 +180,13 @@ namespace fdms {
             if(h_star - h + 1 > 0)
                 ms[ms_idx++] = 1; // ... and a 1
 
-
             if(h_star < ms_size){ // remove prefixes of t[k..h*] until you can extend by 'c'
                 do{ // remove suffixes of t[k..] until you can extend by 'c'
                     v = st.parent(v);
-                    u = st.double_rank_fail_wl_mrep(v, c, (maxrep[v.i] == maxrep[v.j] == 1));
-                } while(st.is_root(u));
+                    //if(maxrep[v.i] == maxrep[v.j] == 1){ // if not bstep would fail
+                        I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                    //}
+                } while(I.first > I.second);
                 h_star += 1;
             }
 
@@ -184,18 +209,18 @@ namespace fdms {
                                           const size_type from, const size_type to){
         size_type k = from, h_star = k + 1, h = h_star, h_star_prev = h_star, k_prim, ms_idx = 0, ms_size = t.size();
         uint8_t c = t[k];
-        node_type v = st.double_rank_fail_wl(st.root(), c), u = v;
+        node_type v = st.double_rank_fail_wl(st.root(), c);
+        Interval I = init_interval(st, c);
 
         while(k < to){
             h = h_star;
             h_star_prev = h_star;
-            u = v;
 
-            while((!st.is_root(u)) && h_star < ms_size){
+            while(I.first <= I.second && h_star < ms_size){
                 c = t[h_star];
-                u = st.double_rank_fail_wl(v, c);
-                if(!st.is_root(u)){
-                    v = u;
+                I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                if(I.first <= I.second){
+                    v = st.double_rank_fail_wl(v, c);
                     h_star += 1;
                 }
             }
@@ -211,8 +236,8 @@ namespace fdms {
             if(h_star < ms_size){ // remove prefixes of t[k..h*] until you can extend by 'c'
                 do{ // remove suffixes of t[k..] until you can extend by 'c'
                     v = st.parent(v);
-                    u = st.double_rank_fail_wl(v, c);
-                } while(st.is_root(u));
+                    I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                } while(I.first > I.second);
                 h_star += 1;
             }
 
@@ -235,18 +260,19 @@ namespace fdms {
                                           const size_type from, const size_type to){
         size_type k = from, h_star = k + 1, h = h_star, h_star_prev = h_star, k_prim, ms_idx = 0, ms_size = t.size();
         uint8_t c = t[k];
-        node_type v = st.double_rank_nofail_wl(st.root(), c), u = v;
+        node_type v = st.double_rank_nofail_wl(st.root(), c);
+        Interval I = init_interval(st, c);
 
         while(k < to){
             h = h_star;
             h_star_prev = h_star;
-            u = v;
 
-            while((!st.is_root(u)) && h_star < ms_size){
+            while(I.first <= I.second && h_star < ms_size){
                 c = t[h_star];
-                u = st.double_rank_nofail_wl(v, c);
-                if(!st.is_root(u)){
-                    v = u;
+                I = bstep_on_interval(st, st.csa.bwt.double_rank(v.i, v.j + 1, c), st.csa.char2comp[c]);
+
+                if(I.first <= I.second){ // if(I.first <= I.second && h_star++)
+                    v = st.double_rank_nofail_wl(v, c);
                     h_star += 1;
                 }
             }
@@ -262,8 +288,8 @@ namespace fdms {
             if(h_star < ms_size){ // remove prefixes of t[k..h*] until you can extend by 'c'
                 do{ // remove suffixes of t[k..] until you can extend by 'c'
                     v = st.parent(v);
-                    u = st.double_rank_nofail_wl(v, c);
-                } while(st.is_root(u));
+                    I = bstep_on_interval(st, st.csa.bwt.double_rank(v.i, v.j + 1, t[h_star]), st.csa.char2comp[t[h_star]]);
+                } while(I.first > I.second);
                 h_star += 1;
             }
 
@@ -287,16 +313,17 @@ namespace fdms {
         size_type k = from, h_star = k + 1, h = h_star, h_star_prev = h_star, k_prim, ms_idx = 0, ms_size = t.size();
         uint8_t c = t[k];
         node_type v = st.lazy_double_rank_wl(st.root(), c); st.lazy_wl_followup(v);
-        node_type u = v;
+        Interval I = init_interval(st, c);
 
         while(k < to){
             h = h_star;
             h_star_prev = h_star;
-            while((!st.is_root(u)) && h_star < ms_size){
+
+            while(I.first <= I.second && h_star < ms_size){
                 c = t[h_star];
-                u = st.lazy_double_rank_wl(v, c);
-                if(!st.is_root(u)){
-                    v = u;
+                I = bstep_on_interval(st, st.csa.bwt.double_rank(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                if(I.first <= I.second){ // if(I.first <= I.second && h_star++)
+                    v = st.lazy_double_rank_wl(v, c);
                     h_star += 1;
                 }
             }
@@ -313,8 +340,10 @@ namespace fdms {
             if(h_star < ms_size){ // remove prefixes of t[k..h*] until you can extend by 'c'
                 do{ // remove suffixes of t[k..] until you can extend by 'c'
                     v = st.parent(v);
-                    u = st.lazy_double_rank_wl(v, c);
-                } while(st.is_root(u));
+                    I = bstep_on_interval(st, st.csa.bwt.double_rank(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                    //u = st.lazy_double_rank_wl(v, c);
+                } while(I.first > I.second);
+                //} while(st.is_root(u));
 
                 h_star += 1;
             }
@@ -339,18 +368,17 @@ namespace fdms {
         size_type k = from, h_star = k + 1, h = h_star, h_star_prev = h_star, k_prim, ms_idx = 0, ms_size = t.size();
         uint8_t c = t[k];
         node_type v = st.lazy_double_rank_fail_wl(st.root(), c); //st.lazy_wl_followup(v);
-        node_type u = v;
+        Interval I = init_interval(st, c);
 
         while(k < to){
             h = h_star;
             h_star_prev = h_star;
-            u = v;
 
-            while((!st.is_root(u)) && h_star < ms_size){
+            while(I.first <= I.second && h_star < ms_size){
                 c = t[h_star];
-                u = st.lazy_double_rank_fail_wl(v, c);
-                if(!st.is_root(u)){
-                    v = u;
+                I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                if(I.first <= I.second){ // if(I.first <= I.second && h_star++)
+                    v = st.lazy_double_rank_fail_wl(v, c);
                     h_star += 1;
                 }
             }
@@ -368,8 +396,10 @@ namespace fdms {
             if(h_star < ms_size){ // remove prefixes of t[k..h*] until you can extend by 'c'
                 do{ // remove suffixes of t[k..] until you can extend by 'c'
                     v = st.parent(v);
-                    u = st.lazy_double_rank_fail_wl(v, c);
-                } while(st.is_root(u));
+                    //u = st.lazy_double_rank_fail_wl(v, c);
+                    I = bstep_on_interval(st, st.csa.bwt.double_rank_and_fail(v.i, v.j + 1, c), st.csa.char2comp[c]);
+                //} while(st.is_root(u));
+                } while(I.first > I.second);
                 h_star += 1;
             }
             
