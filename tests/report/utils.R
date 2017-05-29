082811b78111a85018ffeb994ff81de9118c5e29
Olgert Denas <gertidenas@gmail.com>
2017-05-29 12:47:13 -0700

    unified box plots for performance of tricks

--- tests/report/utils.R
+++ tests/report/utils.R
@@ -8,6 +8,7 @@ read_ds <- function(fname, set_input_type = FALSE){
 
   ds <- read_csv(fname)
   if(set_input_type){
+    stopifnot("b_path" %in% colnames(ds))
     ds$inp_type <- simplify2array(lapply(strsplit(ds$b_path, "_", fixed = TRUE),
                                          parse_b_path))
   }
@@ -20,6 +21,15 @@ sorted_bpaths <- function(b_path){
   data.frame(b_path = b_path,
              mu_nr = as.numeric(simplify2array(lapply(strsplit(b_path, "_", fixed=TRUE), function(l) ifelse(length(l) == 4, l[[4]], NA)))),
              inp_type = sapply(b_path, function(s) ifelse(substr(s, 5, 6) == "1G", "G", "M")))
+}
+
+
+performance_boxplot <- function(p, tit, ylab_){
+  p + geom_boxplot(outlier.shape = NA) +
+    geom_jitter(alpha=0.1, width=0.1, size = 2) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1),
+          legend.position="bottom") +
+    labs(title = tit) + ylab(ylab_)
 }
 
 
