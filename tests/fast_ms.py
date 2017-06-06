0120d9622d6a2df1fca188a123a41f7811fc8ad3
Olgert Denas <gertidenas@gmail.com>
2017-06-06 11:24:15 -0700

    generalize fast_ms.py to what  script_benchmark.py does

--- tests/fast_ms.py
+++ tests/fast_ms.py
@@ -7,29 +7,37 @@ compute matching statistics
 
 
 import logging
+import os
 import sys
 import argparse
 import subprocess
 
-from utils import MsInput, verbose_args, MsInterface
+from utils import verbose_args, MsInterface, get_output
 
 
 logging.basicConfig(level=logging.INFO)
 LG = logging.getLogger(__name__)
 
 
-def get_output(command, *args, **kwargs):
-    LG.debug("running: " + str(command))
-    res = subprocess.check_call(command, *args, shell=True, **kwargs)
-    return res
-
-
 def main(opt):
     logging.getLogger().setLevel(logging.DEBUG if opt.v else logging.INFO)
-    LG.debug("running on S=%s, T=%s", opt.s_path, opt.t_path)
+    if opt.output != '/dev/stdout' and os.path.exists(opt.output):
+        LG.error("output file (%s) exsts. Exiting ...", opt.output)
+        return 1
     command = MsInterface.command_from_dict(vars(opt))
-    LG.debug("running: " + str(command))
-    subprocess.check_call(command, shell=True)
+
+    pref = os.path.basename(opt.s_path).replace(".s", "")
+    header_suff = ",".join(['label', opt.repeat_colname, 'b_path'])
+    for j in range(opt.repeat):
+        with open(opt.output, 'a') as fd:
+            res = get_output(command)
+            if j == 0:
+                fd.write(res[0] + "," + header_suff + "\n")
+            for line in res[1:]:
+                fd.write(line.replace(" ", "") +
+                         ("," + opt.label) +
+                         ("," + str(j + 1)) +
+                         ("," + pref) + "\n")
 
 
 if __name__ == "__main__":
@@ -41,7 +49,14 @@ if __name__ == "__main__":
     for k in MsInterface.params:
         args, kwargs = MsInterface.as_argparse_kwds(k)
         arg_parser.add_argument(*args, **kwargs)
-
     verbose_args(arg_parser)
+    arg_parser.add_argument("--repeat", type=int, default=1,
+                            help="repeat experiment")
+    arg_parser.add_argument("--label", type=str, default='default',
+                            help="ad a label column to output")
+    arg_parser.add_argument("--repeat_colname", type=str, default='ntrial',
+                            help="col label corresponding to the repeat nr.")
+    arg_parser.add_argument("--output", type=str, default='/dev/stdout',
+                            help="output")
 
     sys.exit(main(arg_parser.parse_args()))
