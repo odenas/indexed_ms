61942ee1a3289df29585921c03cf0649d457cd98
Olgert Denas <gertidenas@gmail.com>
2017-09-16 17:34:13 -0700

    sandbox testing of maxrep, rank and fail, double rank

--- fast_ms/sandbox_test_maxrep/main.cpp
+++ fast_ms/sandbox_test_maxrep/main.cpp
@@ -9,7 +9,7 @@
 
 #include <iostream>
 #include "utils.hpp"
-
+#include "cmd_utils.hpp"
 #include "maxrep_construction.hpp"
 #include "stree_sct3.hpp"
 
@@ -17,6 +17,7 @@
 #define NWLCALLS 100000
 #define NTRIALS  5
 #define CALL_MEMBER_FN(object,ptrToMember)  ((object).*(ptrToMember))
+#define IS_MAXIMAL(node) ( ((node).i != (node).j) && (maxrep[(node).i] == 1) && (maxrep[(node).j] == 1) )
 
 using namespace std;
 using namespace fdms;
@@ -194,6 +195,68 @@ void time_fun(StreeOhleb<>& st, string& s, sdsl::bit_vector& maxrep, const size_
     }
 }
 
+void show(const StreeOhleb<>& st, const string& s, const sdsl::bit_vector& maxrep, const string fun_name,
+          const node_type v, const size_type t_micro){
+    
+    cout << (in_same_major_block(v) ? 1 : 0) << "," ; // is close?
+    cout << fun_name << ",";                // method name
+    cout << count_wl(st, v) << "," ;        // how many wl?
+    cout << IS_MAXIMAL(v) << ",";            // is_maximal
+    cout << t_micro << endl;
+}
+
+
+void test_single_rank(const StreeOhleb<>& st, const string& s, const sdsl::bit_vector& maxrep, const size_type max_k){
+    node_type v = st.root(), u = st.root();
+
+    for (size_type k = max_k - 1; k > 0; k--){
+        auto start = timer::now();
+        for(size_type i = 0; i < st.csa.sigma; i++)
+            u = st.single_rank_wl(v, st.csa.comp2char[i]);
+        size_type t = std::chrono::duration_cast<std::chrono::microseconds>(timer::now() - start).count();
+        show(st, s, maxrep, "s", v, t);
+        v = st.double_rank_fail_wl(v, s[k]);
+    }
+}
+
+void test_double_rank(const StreeOhleb<>& st, const string& s, const sdsl::bit_vector& maxrep, const size_type max_k){
+    node_type v = st.root(), u = st.root();
+    
+    for (size_type k = max_k - 1; k > 0; k--){
+        auto start = timer::now();
+        for(size_type i = 0; i < st.csa.sigma; i++)
+            u = st.double_rank_fail_wl(v, st.csa.comp2char[i]);
+        size_type t = std::chrono::duration_cast<std::chrono::microseconds>(timer::now() - start).count();
+        show(st, s, maxrep, "f", v, t);
+        v = st.double_rank_fail_wl(v, s[k]);
+    }
+}
+
+void test_double_rank_nofail(const StreeOhleb<>& st, const string& s, const sdsl::bit_vector& maxrep, const size_type max_k){
+    node_type v = st.root(), u = st.root();
+    
+    for (size_type k = max_k - 1; k > 0; k--){
+        auto start = timer::now();
+        for(size_type i = 0; i < st.csa.sigma; i++)
+            u = st.double_rank_nofail_wl(v, st.csa.comp2char[i]);
+        size_type t = std::chrono::duration_cast<std::chrono::microseconds>(timer::now() - start).count();
+        show(st, s, maxrep, "d", v, t);
+        v = st.double_rank_fail_wl(v, s[k]);
+    }
+}
+
+void test_mrep_rank(const StreeOhleb<>& st, const string& s, const sdsl::bit_vector& maxrep, const size_type max_k){
+    node_type v = st.root(), u = st.root();
+    
+    for (size_type k = max_k - 1; k > 0; k--){
+        auto start = timer::now();
+        for(size_type i = 0; i < st.csa.sigma; i++)
+            u = st.double_rank_fail_wl_mrep(v, st.csa.comp2char[i], IS_MAXIMAL(v));
+        size_type t = std::chrono::duration_cast<std::chrono::microseconds>(timer::now() - start).count();
+        show(st, s, maxrep, "m", v, t);
+        v = st.double_rank_fail_wl(v, s[k]);
+    }
+}
 
 int main(int argc, char **argv) {
     OptParser input(argc, argv);
@@ -212,22 +275,12 @@ int main(int argc, char **argv) {
     size_type mrep_time = fdms::load_maxrep(maxrep, st, s, s_spec.rev_maxrep_fname, flags.load_maxrep);
     cerr << "DONE ( " << mrep_time << " milliseconds)" << endl;
 
-    cout << "len_s,nwlcalls,ntrial,close,method,char,charidx,has_wl,is_maximal,time_ms" << endl;
-    cerr << " * testing single rank" << endl;
-    time_fun(st, s, maxrep, NWLCALLS, (s.size() - 1 > NTRIALS ? NTRIALS : s.size() - 1),
-             &StreeOhleb<>::single_rank_wl, &StreeOhleb<>::double_rank_fail_wl_mrep, 1, "single_rank");
-
-    cerr << " * testing double rank and fail" << endl;
-    time_fun(st, s, maxrep, NWLCALLS, (s.size() - 1 > NTRIALS ? NTRIALS : s.size() - 1),
-             &StreeOhleb<>::double_rank_fail_wl, &StreeOhleb<>::double_rank_fail_wl_mrep, 1, "double_rank_fail");
-
-    cerr << " * testing double rank with maxrep" << endl;
-    time_fun(st, s, maxrep, NWLCALLS, (s.size() - 1 > NTRIALS ? NTRIALS : s.size() - 1),
-             &StreeOhleb<>::double_rank_fail_wl, &StreeOhleb<>::double_rank_fail_wl_mrep, 2, "double_rank_maxrep");
-
-    cerr << " * testing double rank with maxrep; fast" << endl;
-    time_fun(st, s, maxrep, NWLCALLS, (s.size() - 1 > NTRIALS ? NTRIALS : s.size() - 1),
-             &StreeOhleb<>::double_rank_fail_wl, &StreeOhleb<>::double_rank_fail_wl_mrep, 3, "double_rank_maxrep_fast");
-
+    size_type max_k = st.size() / 100;
+    cout << "close,method,has_wl,is_maximal,time_micro" << endl;
+    test_single_rank(st, s, maxrep, max_k);
+    test_double_rank(st, s, maxrep, max_k);
+    test_double_rank_nofail(st, s, maxrep, max_k);
+    test_mrep_rank(st, s, maxrep, max_k);
+    
     return 0;
 }
