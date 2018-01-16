8f612e3add68726700f08ff23c76711a74fd0526
Olgert Denas <gertidenas@gmail.com>
2018-01-15 23:49:15 -0800

    analysis draft of input exploration

--- tests/report/utils.R
+++ tests/report/utils.R
@@ -1,47 +1,32 @@
-library(tidyverse)
-library(knitr)
 
-read_ds <- function(fname, set_input_type = FALSE){
-  ds <- read_csv(fname)
-  if(set_input_type){
-    stopifnot("b_path" %in% colnames(ds))
-    ds <- (ds %>% separate(b_path, into=c("inp_type", "sl", "tl", "alp", "mut_rate")))
-  }
-  ds
+
+read_many_csv <- function(flist, add_bpath=TRUE){
+  do.call(rbind, lapply(flist, function(f) {
+    if(add_bpath)
+      read_csv(f) %>% mutate(b_path=basename(f))
+    else
+      read_csv(f)
+  }))
 }
 
-
-sorted_bpaths <- function(b_path){
-  b_path <- as.character(levels(factor(b_path)))
-  data.frame(b_path = b_path,
-             mu_nr = as.numeric(simplify2array(lapply(strsplit(b_path, "_", fixed=TRUE), function(l) ifelse(length(l) == 4, l[[4]], NA)))),
-             inp_type = sapply(b_path, function(s) ifelse(substr(s, 5, 6) == "1G", "G", "M")))
+performance_boxplot <- function(p, tit){
+  p + geom_boxplot(outlier.shape = 2, outlier.size = 0.5, outlier.colour = 'black') +
+    geom_jitter(alpha=0.2, width=0.1) +
+    theme(#axis.text.x = element_text(angle = 0, hjust = 1),
+      legend.position="bottom") +
+    labs(title = tit) + ylab("time_ms")
 }
 
+prange_plot <- function(dt){
+  qup <- function(x) quantile(x, 3/4)
+  qdown <- function(x) quantile(x, 1/4)
 
-performance_boxplot <- function(p, tit, ylab_){
-  p + geom_boxplot(outlier.shape = NA) +
-    geom_jitter(alpha=0.1, width=0.1, size = 2) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 1),
-          legend.position="bottom") +
-    labs(title = tit) + ylab(ylab_)
+  ggplot(dt, aes(inp_type, rel_diff)) +
+    geom_pointrange(stat = "summary", fatten = 1, fun.y = median, fun.ymin = qdown, fun.ymax = qup) +
+    geom_jitter(alpha=0.1, width = 0.1) +
+    geom_hline(yintercept = 0.0, color='blue') +
+    facet_grid(~alp) +
+    ylim(-1, 1)
 }
-
-
-# input_description <- function(slen, tlen, itype, mperiod){
-#   frm_len <- function(n){
-#     if (n < 1000) sprintf("%d", n)
-#     else if (n < 100000) sprintf("%dKB", round(n / 1000))
-#     else if (n < 100000000) sprintf("%dMB", round(n / 1000000))
-#     else sprintf("%dGB", round(n / 1000000000))
-#   }
-#
-#   if(substr(itype, 1, 3) == "rnd")
-#     sprintf("random with |s| = %s and |t| = %s", frm_len(slen), frm_len(tlen))
-#   else
-#     sprintf("mutated (every %d chars) with |s| = %s and |t| = %s", mperiod, frm_len(slen), frm_len(tlen))
-# }
-#
-
 
 
