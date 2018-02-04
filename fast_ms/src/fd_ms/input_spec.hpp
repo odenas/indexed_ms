59e52276464e3e1f832f17fd448a386f6d651180
Olgert Denas <gertidenas@gmail.com>
2018-02-04 14:59:14 -0800

    dump all dstructs at once and new code test directory

--- fast_ms/src/fd_ms/input_spec.hpp
+++ fast_ms/src/fd_ms/input_spec.hpp
@@ -15,21 +15,26 @@ using namespace std;
 namespace fdms{
     class InputSpec{
     public:
-        string s_fname, fwd_cst_fname, rev_cst_fname, rev_maxrep_fname, rev_elst_fname, fwd_nwdlst_fname;
+        string s_fname; 
+        string fwd_cst_fname, rev_cst_fname; 
+        string fwd_maxrep_fname, rev_maxrep_fname;
+        string rev_elst_fname;
+        string fwd_nwdlst_fname;
         
         InputSpec(){}
         
         InputSpec(const InputSpec &is) :
-        s_fname{is.s_fname},
-        fwd_cst_fname{is.fwd_cst_fname},
-        rev_cst_fname{is.rev_cst_fname},
-        rev_maxrep_fname{is.rev_maxrep_fname},
-        rev_elst_fname{is.rev_elst_fname},
-        fwd_nwdlst_fname{is.fwd_nwdlst_fname} {}
+            s_fname{is.s_fname},
+            fwd_cst_fname{is.fwd_cst_fname},
+            rev_cst_fname{is.rev_cst_fname},
+            rev_maxrep_fname{is.rev_maxrep_fname},
+            rev_elst_fname{is.rev_elst_fname},
+            fwd_nwdlst_fname{is.fwd_nwdlst_fname} {}
         
         InputSpec(string s_fn) : s_fname(s_fn){
             fwd_cst_fname = s_fname + ".fwd.stree";
             rev_cst_fname = s_fname + ".rev.stree";
+            fwd_maxrep_fname = s_fname + ".fwd.maxrep";
             rev_maxrep_fname = s_fname + ".rev.maxrep";
             rev_elst_fname = s_fname + ".rev.elst";
             fwd_nwdlst_fname = s_fname + ".fwd.nwdlst";
@@ -40,6 +45,7 @@ namespace fdms{
                 s_fname = string(other.s_fname);
                 fwd_cst_fname = string(other.fwd_cst_fname);
                 rev_cst_fname = string(other.rev_cst_fname);
+                fwd_maxrep_fname = string(other.fwd_maxrep_fname);
                 rev_maxrep_fname = string(other.rev_maxrep_fname);
                 rev_elst_fname = string(other.rev_elst_fname);
                 fwd_nwdlst_fname = string(other.fwd_nwdlst_fname);
---
+++ fast_ms/tests/mstat_script_tests.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+"""
+Test whether programs produce the same output
+"""
+
+import logging
+import sys
+import os
+from difflib import ndiff
+
+from mstat.interface import default_arg_parser, get_output, FdMsInterface
+from mstat.dataset import InputPair
+
+
+logging.basicConfig(level=logging.INFO)
+LG = logging.getLogger()
+
+
+def slow(load_dir, ms_input):
+    s_path, t_path = ms_input
+    if load_dir is None:
+        return ("mstat_slow_ms.py {s_path} {t_path}".format(**locals()))
+    load_path = os.path.join(load_dir, os.path.basename(s_path).replace(".s", ".mstat"))
+    return ("cat {load_path}".format(**locals()))
+
+
+def fast(opt, ms_input):
+    params = dict(lazy_wl=opt.lazy_wl,
+                  rank_fail=opt.rank_fail,
+                  double_rank=opt.double_rank,
+                  lca_parents=opt.lca_parents,
+                  use_maxrep_vanilla=opt.use_maxrep_vanilla,
+                  use_maxrep_rc=opt.use_maxrep_rc,
+                  answer=opt.answer,
+                  avg=opt.avg,
+                  s_path=ms_input.s_path, t_path=ms_input.t_path)
+    return FdMsInterface.command_from_dict(params)
+
+
+def check_res(res1, res2, avg):
+    if avg:
+        res1 = float(res1[0])
+        res2 = [int(_) for _ in res2[0].split()]
+        res2 = sum(res2) / float(len(res2))
+        return ["fast (%.4f) != slow (%.4f)" % (res1, res2)
+                if round(res1, 4) != round(res2, 4) else False]
+
+    error_lst = []
+    for i, (o1, o2) in enumerate(zip(res1, res2)):
+        for line in ndiff(o1.split(), o2.split()):
+            if line[:2] == '  ':
+                error_lst.append(False)
+            else:
+                error_lst.append(line)
+    return error_lst
+
+
+def main(opt):
+    logging.getLogger().setLevel(logging.DEBUG if opt.verbose else logging.INFO)
+
+    for pref in opt.prefixes:
+        ms_input = InputPair.basedir_form(opt.base_dir, pref)
+        LG.info("testing on input %s", ms_input)
+
+        res1 = get_output(fast(opt, ms_input))
+        res2 = get_output(slow(opt.slow_load_dir, ms_input))
+
+        err_lst = check_res(res1, res2, opt.avg)
+        if any(err_lst):
+            for err in err_lst:
+                if err:
+                    print("\t" + err)
+        print
+        if any(err_lst):
+            raise ValueError("ended with errors")
+
+
+if __name__ == "__main__":
+    arg_parser = default_arg_parser(__doc__)
+    arg_parser.add_argument("base_dir", type=str, help="base dir")
+    arg_parser.add_argument("prefixes", type=str, nargs="+",
+                            help="input prefixes")
+
+    arg_parser.add_argument("--slow_load_dir", type=str, default=None,
+                            help="load results of slow program")
+
+    for k in ('double_rank', 'lazy_wl', 'rank_fail', 'use_maxrep_rc', 'use_maxrep_vanilla',
+              'load_cst', 'load_maxrep',
+              'lca_parents', 'avg', 'answer'):
+        args, kwargs = FdMsInterface.as_argparse_kwds(k)
+        arg_parser.add_argument(*args, **kwargs)
+    sys.exit(main(arg_parser.parse_args()))
