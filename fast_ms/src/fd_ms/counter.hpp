acc6d672c09b6a5a8046f4a6a364a53b297f675f
Olgert Denas <gertidenas@gmail.com>
2018-05-21 23:10:18 -0700

    place counter to its own file

---
+++ fast_ms/src/fd_ms/counter.hpp
@@ -0,0 +1,31 @@
+/**
+ * helper data structure for keeping counts of things
+ */
+
+#ifndef counter_h
+#define counter_h
+
+#include <map>
+#include <string>
+#include <chrono>
+
+
+using namespace std;
+using timer = std::chrono::high_resolution_clock;
+
+namespace fdms{
+    template<typename size_type>
+    class Counter{
+	    public:
+	    map<string, size_type> reg;
+
+	    Counter(){}
+
+	    void register_now(const string key, const std::chrono::time_point<std::chrono::_V2::system_clock, std::chrono::nanoseconds> start){
+		    auto end = timer::now();
+		    reg[key] = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		    cerr << "DONE (" << reg[key] / 1000 << " seconds)" << endl;
+	    }
+    };
+};
+#endif 
