d305b782eddbf6c2630ef27010832bd6c9b9f5e8
Olgert Denas <gertidenas@gmail.com>
2020-03-02 21:41:15 -0800

    implement trivial noindex rrr

--- fast_ms/src/fd_ms/partial_max_vector.hpp
+++ fast_ms/src/fd_ms/partial_max_vector.hpp
@@ -22,35 +22,6 @@ namespace fdms {
     protected:
         typedef sdsl::int_vector_buffer<1> buff_vec_t;
         typedef Counter<size_type> counter_t;
-
-        size_type _slow_indexed_range_max_prefix(const sdsl::int_vector<64>& ridx, const size_type int_to, const size_type bsize) const {
-            size_type int_ms_idx = int_to;
-            size_type bit_ms_idx = m_ms_sel(int_ms_idx + 1);
-            size_type block_idx = bit_ms_idx / bsize;
-
-            size_type sum_ms = 0; // to be subtracted from ridx[block_idx]
-            {
-                size_type prev_ms = bit_ms_idx - (2 * int_ms_idx); // needed for 1st term beyond the sum
-                size_type nzeros = 0;
-
-                // loop from bit_ms_idx + 1 to the end_of_block
-                size_type end_of_block = std::min<size_type>(m_ms.size(), (block_idx + 1) * bsize);
-                for (size_type i = bit_ms_idx + 1; i < end_of_block; i++) {
-                    if (m_ms[i] == 1) {
-                        size_type cur_ms = (prev_ms + nzeros - 1);
-                        sum_ms += cur_ms; // since MS_i - MS_{i-1} + 1 = nzeros
-                        prev_ms = cur_ms;
-                        nzeros = 0;
-                    } else {
-                        nzeros += 1;
-                    }
-                }
-            }
-            size_type answer = ridx[block_idx] - sum_ms;
-            return answer;
-        }
-
-
     public:
         vec_type m_ms;
         ms_sel_1_type m_ms_sel;
@@ -133,10 +104,6 @@ namespace fdms {
 
         none_partial_max_vector(const string& ms_path, counter_t& time_usage) : base_cls(ms_path, time_usage) {}
 
-        size_type check_range_max(const size_type int_from, const size_type int_to) const {
-            return base_cls::trivial_range_max(int_from, int_to);
-        }
-
         size_type noindex_range_max(const size_type  int_from, const size_type int_to, const IndexedAlgorithm algo) const {
             if (int_from >= int_to)
                 return 0;
@@ -147,6 +114,37 @@ namespace fdms {
                 size_type prev_ms = bit_from - 2 * int_from;
                 //return (size_type) ms_range_max_fast64(prev_ms, bit_from, bit_to, this->m_ms.data());
                 throw string{"Not supported yet"};
+            } else if (algo == IndexedAlgorithm::trivial) {
+                return base_cls::trivial_range_max(int_from, int_to);
+            }
+        }
+    };
+
+    template <typename size_type>
+    class rrr_partial_max_vector : sdsl_partial_max_vector<sdsl::rrr_vector<>, sdsl::rrr_vector<>::select_1_type, size_type>{
+        inline uint64_t uncompress64(const size_type word_from) const {
+            uint64_t bit_from = word_from * 64;
+            uint8_t width = std::min<uint64_t>(64, this->m_ms.size() - bit_from);
+            return this->m_ms.get_int(bit_from, width);
+        }
+    public:
+        typedef sdsl_partial_max_vector<sdsl::rrr_vector<>, sdsl::rrr_vector<>::select_1_type, size_type>  base_cls;
+        typedef typename base_cls::counter_t counter_t;
+
+        rrr_partial_max_vector(const string& ms_path) : base_cls(ms_path) {}
+
+        rrr_partial_max_vector(const string& ms_path, counter_t& time_usage) : base_cls(ms_path, time_usage) {}
+
+        size_type noindex_range_max(const size_type  int_from, const size_type int_to, const IndexedAlgorithm algo) const {
+            if (int_from >= int_to)
+                return 0;
+
+            if(algo == IndexedAlgorithm::djamal){
+                size_type bit_from = this->m_ms_sel(int_from + 1);
+                size_type bit_to = this->m_ms_sel(int_to);
+                size_type prev_ms = bit_from - 2 * int_from;
+                throw string{"not supported"};
+                //return _bit_djamal_range_sum_fast(bit_from, bit_to, prev_ms);
             } else if (algo == IndexedAlgorithm::trivial) {
                 return base_cls::trivial_range_max(int_from, int_to);
             }
