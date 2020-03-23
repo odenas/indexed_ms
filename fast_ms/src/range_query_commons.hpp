072353c995500699042c341907153a98f66a441f
Olgert Denas <gertidenas@gmail.com>
2020-03-22 18:13:57 -0700

    interim

--- fast_ms/src/range_query_commons.hpp
+++ fast_ms/src/range_query_commons.hpp
@@ -160,38 +160,43 @@ namespace fdms {
         static void none_indexed_profile(const string ms_path, const string ridx_path, const size_type nqueries,
                 const size_type range_size, const size_type from_idx_max,
                 const int block_size, counter_t& time_usage, const IndexedAlgorithm algo, const RangeOperation op){
-            if(op == RangeOperation::r_max)
-                throw string{"Operation max not implemented with index."};
-
-
-            none_partial_sums_vector<size_type> psum(ms_path, time_usage);
 
             sdsl::int_vector<64> ridx;
             _load_time_ridx(ridx, ridx_path, time_usage);
 
-            auto comp_start = timer::now();
-            for (int k = 0; k < nqueries; k++) {
-                size_type start = random_index(from_idx_max);
-                psum.indexed_range_sum(ridx, start, start + range_size, (size_type) block_size, algo);
-            }
-            time_usage.register_now("algorithm", comp_start);
+            if(op == RangeOperation::r_max){
+                none_partial_max_vector<size_type> pmax(ms_path, time_usage);
+                auto comp_start = timer::now();
+                for (int k = 0; k < nqueries; k++) {
+                    size_type start = random_index(from_idx_max);
+                    cerr << start << ", " << start + range_size << endl;
+                    pmax.indexed_range_max(ridx, start, start + range_size, (size_type) block_size, algo);
+                }
+                time_usage.register_now("algorithm", comp_start);
+            } else if (op == RangeOperation::r_sum) {
+                none_partial_sums_vector<size_type> psum(ms_path, time_usage);
+                auto comp_start = timer::now();
+                for (int k = 0; k < nqueries; k++) {
+                    size_type start = random_index(from_idx_max);
+                    psum.indexed_range_sum(ridx, start, start + range_size, (size_type) block_size, algo);
+                }
+                time_usage.register_now("algorithm", comp_start);
+            } else
+                throw string{"Operation max not implemented with index."};
         }
         static size_type none_indexed(const string ms_path, const string ridx_path,
                 const size_type from_idx, const size_type to_idx, const int block_size,
                 const bool check, const IndexedAlgorithm algo, const RangeOperation op){
             counter_t  time_usage;
+            sdsl::int_vector<64> ridx;
+            sdsl::load_from_file(ridx, ridx_path);
+
             if(op == RangeOperation::r_max){
                 none_partial_max_vector<size_type> pmax(ms_path, time_usage);
-
-                sdsl::int_vector<64> ridx;
-                sdsl::load_from_file(ridx, ridx_path);
                 size_type answer = pmax.indexed_range_max(ridx, from_idx, to_idx, (size_type) block_size, algo);
                 return (check ? __check_outcome(answer, pmax.check_range_max(from_idx, to_idx)) : answer);
             } else {
                 none_partial_sums_vector<size_type> psum(ms_path, time_usage);
-
-                sdsl::int_vector<64> ridx;
-                sdsl::load_from_file(ridx, ridx_path);
                 size_type answer = psum.indexed_range_sum(ridx, from_idx, to_idx, (size_type) block_size, algo);
                 return (check ? __check_outcome(answer, psum.check_range_sum(from_idx, to_idx)) : answer);
             }
