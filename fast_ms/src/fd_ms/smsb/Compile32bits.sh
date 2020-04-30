6a1e5e7aa70b5cffd68c7ed28a067ab05a57b912
olgert denas <gertidenas@gmail.com>
2020-04-30 00:12:31 -0700

    some installation loose ends

--- fast_ms/src/fd_ms/smsb/Compile32bits.sh
+++
@@ -1,1 +0,0 @@
-gcc test_range_ms.c range_ms_sum.c naive_ms_range_sum.c range_ms_tables.c mt19937ar.c -o test_range_ms -O3 -Wall
--- fast_ms/src/fd_ms/smsb/Compile64bits.sh
+++
@@ -1,1 +0,0 @@
-gcc test_range_ms64.c range_ms_sum.c naive_ms_range_sum.c range_ms_tables.c mt19937ar.c -o test_range_ms64 -O3 -Wall
