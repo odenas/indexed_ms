393a540fbd250252dbc234c057990ed85d2cfb99
Olgert Denas <gertidenas@gmail.com>
2021-06-27 23:41:43 -0700

    ployground

---
+++ fast_ms/code_playground/no_diff_compress/diff_none_compress.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+TABLES=../../bin/diff_none_tables.x
+COMPRESS=../../bin/diff_none_compress_ms.x
+
+THRESHOLD=5  # Threshold on MS values, as above.
+NEGATIVE=0
+GREEDY=0  # Greedy strategy for too large windows. Either 0 or 1.
+N_ZEROS=300  # Max n. of zeros in a precomputed table
+N_ONES=300  # Max n. of ones in a precomputed table
+
+TABLES_DIR=./tables
+OUTPUT_DIR=.
+LOG_FILE=log_file.txt
+MS_FILE=inp.s_inp.t.ms
+
+${COMPRESS} -ms_path ${MS_FILE} -compression rle -outputDir ${OUTPUT_DIR} -tablesDir ${TABLES_DIR} \
+	-threshold ${THRESHOLD} -nZeros ${N_ZEROS} -nOnes ${N_ONES} -negative ${NEGATIVE} -greedy ${GREEDY} -verbose 0  1> ${LOG_FILE} 2>&1
+
