833c7baef13ea85629de263ff818dde4db73f72f
Olgert Denas <gertidenas@gmail.com>
2022-01-15 15:52:11 +0100

    ms wrapper removs temp files

--- fast_ms/wrappers/ms/wrapper.py
+++ fast_ms/wrappers/ms/wrapper.py
@@ -18,9 +18,20 @@ def _exists(which):
     except AttributeError:
         return False
 
+def _try_remove(file_path):
+    file_path = Path(file_path)
+
+    if file_path.is_file():
+        file_path.unlink()
+        if not file_path.is_file():
+            log.info("* removed %s", file_path)
+
 
 load_cst = int(_exists('fwd_cst') and _exists('rev_cst'))
 load_maxrep = int(_exists("maxrep"))
+if len(snakemake.output) != 1:
+    raise AttributeError("Expecting a single output, the .ms file, got %d instead: %s",
+                         len(snakemake.output), str(snakemake.output))
 
 
 cmd = (
@@ -33,4 +44,20 @@ cmd = (
 log.info(format(cmd))
 
 shell(cmd)
+
+if hasattr(snakemake.params, "remove_temp") and snakemake.params.remove_temp:
+    file_id = str(snakemake.output)[:-3]
+    log.info("removing temporary files starting with %s ...", file_id)
+    _try_remove(file_id + ".runs")
+
+    for i in range(snakemake.threads):
+        _try_remove(file_id + (".ms.%d" % i))
+        _try_remove(file_id + (".runs.%d" % i))
+        _try_remove(file_id + (".runs.%d_%d" % (i, i + 1)))
+
+if hasattr(snakemake.params, "remove_idx") and snakemake.params.remove_idx:
+    file_id = str(snakemake.input.s)
+    _try_remove(file_id + ".fwd.stree")
+    _try_remove(file_id + ".rev.stree")
+    _try_remove(file_id + ".rev.maxrep")
 
